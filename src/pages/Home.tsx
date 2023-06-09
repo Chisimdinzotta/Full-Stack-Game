f567169c2ca772baf8180335383bff011eafcf50
Chisimdinzotta <108879532+Chisimdinzotta@users.noreply.github.com>
2023-06-10 00:52:31 +0100

    Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import ticTacToeImage from '../images/tic-tac-toe.png';
+export default function Home(){
+    return(
+        <div className='mainContainer'>
+            <img src = {ticTacToeImage}/>
+            <div className='buttonContainer'>
+                <button className='singlePlayerButton'>Single Player</button>
+                <button className='multiplayerButton'> Multiplayer</button>
+                <button className='localMultiplayerButton'>Local Multiplayer</button>
+            </div>
+        </div>
+    )
+}
