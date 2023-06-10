9c199e79865303bff499f2fa36f8ae89ba439f92
Chisimdinzotta <108879532+Chisimdinzotta@users.noreply.github.com>
2023-06-10 13:21:18 +0100

    Update Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import ticTacToeImage from '../images/tic-tac-toe.png';
 export default function Home(){
     return(
         <div className='mainContainer'>
-            <img src = {ticTacToeImage}/>
+            <img src = {ticTacToeImage} alt='Tic Tac Toe Image'/>
             <div className='buttonContainer'>
                 <button className='singlePlayerButton'>Single Player</button>
                 <button className='multiplayerButton'> Multiplayer</button>
